888b1a86c8418becb4b3dfbec2c4caadbf7e2a4b
Karol Was <kw.kwaq@gmail.com>
2020-07-30 10:21:35 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+`alert('Siemano!')`
+
+const firstName = 'Karol';
+const age = 25;
+let years = 3;
+const futureAge = age + years;
+
+console.log(`Cześć nazywam się ${firstName} obecnie mam ${age}lat ale za ${years} lata będę miał już ${age + years } lat/a.`);
